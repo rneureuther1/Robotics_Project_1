2517c3e4d470c024993f98a03a6cb415edd4b316
rneureuther1 <rneureuther1@users.noreply.github.com>
2015-04-19 11:16:55 -0400

    Update RobotControl.js

--- RobotControl.js
+++ RobotControl.js
@@ -50,7 +50,7 @@ function handler (req, res) {
 function forward()
 {
   console.log("Moving Forward");
-  b.analogWrite(driveMotorFR, 0.9, 10000);
+  b.analogWrite(driveMotorFR, 0.9, 1000);
   
   //setTimeout(function(){b.analogWrite(driveMotorFR,0);}, 4000);
   
@@ -59,7 +59,7 @@ function forward()
 function reverse()
 {
   console.log("Backing Up");
-  b.analogWrite(driveMotorFR, 0.9 , 5000);
+  b.analogWrite(driveMotorFR, 0.9 , 2000);
 }
 
 function turnright()
