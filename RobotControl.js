873261869da3736e1d331ec85641b878865ada09
rneureuther1 <rneureuther1@users.noreply.github.com>
2015-04-19 12:01:18 -0400

    Update RobotControl.js

--- RobotControl.js
+++ RobotControl.js
@@ -24,6 +24,9 @@ var driveMotorFR = "P9_14";
 var driveMotorBR = "P9_16";
 var driveMotorFL = "P9_21";
 var driveMotorBL = "P9_22";
+var duty_min = 0.03;
+var position = 0;
+var increment = 0.1;
 
 b.pinMode(driveMotorFR, b.OUTPUT);
 
@@ -46,6 +49,36 @@ function handler (req, res) {
   });
   console.log("Webpage Loaded");
 }
+
+function updateDuty() {
+    // compute and adjust duty_cycle based on
+    // desired position in range 0..1
+    var duty_cycle = (position*0.115) + duty_min;
+    b.analogWrite(driveMotorFR, duty_cycle, 60, scheduleNextUpdate);
+    console.log("Duty Cycle: " + 
+        parseFloat(duty_cycle*100).toFixed(1) + " %");   
+}
+
+function scheduleNextUpdate() {
+    // adjust position by increment and 
+    // reverse if it exceeds range of 0..1
+    position = position + increment;
+    if(position < 0) {
+        position = 0;
+        increment = -increment;
+    } else if(position > 1) {
+        position = 1;
+        increment = -increment;
+    }
+    
+    // call updateDuty after 200ms
+    setTimeout(updateDuty, 200);
+}
+
+
+
+
+
 
 function forward()
 {
@@ -71,6 +104,7 @@ function turnright()
 function turnleft()
 {
   console.log("Turning Left");
+  b.analogWrite(driveMotorFR, , 60);
 }
 
 function STOP()
